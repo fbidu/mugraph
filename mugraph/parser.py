a6249255f09c7644d4d5617578dfd344a71cd82f
Felipe Rodrigues <felipe@felipevr.com>
2020-06-10 10:00:44 -0300

    Refactor parse method
    This commit breaks the main parsing loop into closures in order
    to improve code readability

--- mugraph/parser.py
+++ mugraph/parser.py
@@ -68,87 +68,88 @@ class Parser:
             if level == 1:
                 self.graph.graph["description"] = content
 
+        def process_heading(token):
+            nonlocal level, last_level, at_consumes
+            text = token["children"][0]["text"]
+            level = token["level"]
+
+            """
+            *** Skip-level checking ***
+            The headers within a session should increase by one step
+            at the time. That is, this is invalid:
+
+                # Title
+
+                ## Consumes
+                #### ERROR
+
+            But the header may not _always_ be incremental:
+
+                # Title
+
+                ## Consumes
+                ### Hey hey
+                ### I'm valid!
+
+            And they can decrease sharply to another H2:
+                # Title
+
+                ## Consumes
+                ### Hey hey
+                #### I have
+                ##### TONS
+                ###### Of details
+
+                ## Produces
+                ### This is valid, btw
+
+            """
+
+            if level != 2 and level not in (last_level, last_level + 1):
+                exception = (
+                    f"Current level {level} is incompatible with "
+                    f"last level {last_level}"
+                )
+                raise ParserException(exception)
+
+            last_level = level
+
+            """
+            *** Header Checking ***
+
+            There should be only one top header
+            """
+            if level == 1:
+                if self.root_name:
+                    raise ParserException("Duplicated header")
+                self.root_name = text
+
+            """
+            *** Produces-Consumes Checking ***
+
+            The only allowed titles at second level headers is "produces"
+            and "consumes". This way we can keep a simple "at_consumes"
+            boolean flag to check where we should put the subsequent
+            texts.
+            """
+            if level == 2:
+                if text.lower() not in ("consumes", "produces"):
+                    raise ParserException(f"'{text}' not allowed as a level two header")
+
+                at_consumes = bool(text.lower() == "consumes")
+
+            if level == 3:
+                if at_consumes:
+                    self.consumes.add(text)
+                else:
+                    self.produces.add(text)
+
         for token in self.tokens:
 
             # The most import processing happens when the token is a heading
             # That is, it starts with at least one # char
             if token.get("type") == "heading":
-
-                text = token["children"][0]["text"]
-                level = token["level"]
-
-                """
-                *** Skip-level checking ***
-                The headers within a session should increase by one step
-                at the time. That is, this is invalid:
-
-                  # Title
-
-                  ## Consumes
-                  #### ERROR
-
-                But the header may not _always_ be incremental:
-
-                    # Title
-
-                    ## Consumes
-                    ### Hey hey
-                    ### I'm valid!
-
-                And they can decrease sharply to another H2:
-                    # Title
-
-                    ## Consumes
-                    ### Hey hey
-                    #### I have
-                    ##### TONS
-                    ###### Of details
-
-                    ## Produces
-                    ### This is valid, btw
-
-                """
-
-                if level != 2 and level not in (last_level, last_level + 1):
-                    exception = (
-                        f"Current level {level} is incompatible with "
-                        f"last level {last_level}"
-                    )
-                    raise ParserException(exception)
-
-                last_level = level
-
-                """
-                *** Header Checking ***
-
-                There should be only one top header
-                """
-                if level == 1:
-                    if self.root_name:
-                        raise ParserException("Duplicated header")
-                    self.root_name = text
-
-                """
-                *** Produces-Consumes Checking ***
-
-                The only allowed titles at second level headers is "produces"
-                and "consumes". This way we can keep a simple "at_consumes"
-                boolean flag to check where we should put the subsequent
-                texts.
-                """
-                if level == 2:
-                    if text.lower() not in ("consumes", "produces"):
-                        raise ParserException(
-                            f"'{text}' not allowed as a level two header"
-                        )
-
-                    at_consumes = bool(text.lower() == "consumes")
-
-                if level == 3:
-                    if at_consumes:
-                        self.consumes.add(text)
-                    else:
-                        self.produces.add(text)
+                process_heading(token)
 
             # Text tokens may contain metadata, schema and other information
             # on nodes and edges
